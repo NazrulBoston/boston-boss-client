31e39e78bab81c4afe8a4e8b3f8e6176d78a5125
Nazrul Islam <dobon.islam@icloud.com>
2023-11-21 09:27:49 -0500

    online order done

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+
+const PopularMenu = () => {
+    const[menu, setMenu] = useState([])
+
+useEffect( () => {
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => {
+        const popularItems = data.filter(item => item.category === 'popular' )
+        setMenu(popularItems)} )
+},[])
+
+
+    return (
+        <section className="mb-12">
+            <SectionTitle
+            heading={"from our menu"}
+            subHeading={"Popular Items"}
+            
+            ></SectionTitle>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+                {
+                    menu.map(item => <MenuItem
+                    key={item._id}
+                    item={item}
+                    ></MenuItem>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default PopularMenu;
